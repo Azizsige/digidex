6af4f9242db0fcb9e0be569c8b0d62ebee3556ab
M. Aziz Sige Kurniawan <46107051+Azizsige@users.noreply.github.com>
2021-11-11 16:46:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const btnSearch = document.querySelector("#btn-search");
+const input = document.querySelector("#input");
+const listDigimonEl = document.querySelector("#content-digimon");
 
 function getDigimon() {
     const xhr = new XMLHttpRequest();
@@ -30,13 +33,33 @@ xhr.open("GET", "https://digimon-api.vercel.app/api/digimon");
      xhr.send();
 }
 
+function getDigimonInput(){
+  const xhr = new XMLHttpRequest();
+  
+  xhr.onload = function(){
+    const responseJson = JSON.parse(this.responseText)
+    
+    if(responseJson.error){
+      showResponseMessage(responseJson.message)
+      toggleLoading(false)
+    } else {
+     renderDigimonInput(responseJson)
+      console.log(responseJson)
+      toggleLoading(true)
+    }
+  }
+  
+  xhr.onerror = function(){
+    showResponseMessage()
+  }
+  
+  xhr.open("GET", `https://digimon-api.vercel.app/api/digimon/name/${input.value}`);
+  xhr.send();
+}
+
 const renderAllDigimons= (digimons) => {
-        const listDigimonEl = document.querySelector("#content-digimon");
 listDigimonEl.innerHTML = ""
-
-
-
-        digimons.forEach(digimon => {
+      digimons.forEach(digimon => {
             listDigimonEl.innerHTML += `
     <div id="content-digimon__card">
        <img src="${digimon.img}" alt="" />
@@ -49,13 +72,37 @@ listDigimonEl.innerHTML = ""
        });
     };
     
-    function toggleLoading(show){
+    
+const renderDigimonInput= (digimons) => {
+listDigimonEl.innerHTML = ""
+      digimons.forEach(digimon => {
+            listDigimonEl.innerHTML += `
+    <div id="content-digimon__card">
+       <img src="${digimon.img}" alt="" />
+    <div id="content-digimon__card--desc">
+  <h5>Name : ${digimon.name}</h5>
+       <h5>Level : ${digimon.level}</h5>
+     </div>
+   </div>
+           `;
+       });
+
+    };
+
+
+
+function toggleLoading(show){
       if(show){
         return document.querySelector("#loading").classList.add("hide")
       } else {
         return document.querySelector("#loading").classList.remove("hide")
       }
     }
+    
+btnSearch.addEventListener('click', function(){
+  getDigimonInput()
+  console.log(input.value)
+})
 
 getDigimon()
  
